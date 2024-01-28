f09caae3e661d4f3cc903f38f907303afb780e13
John Dungao <johnmiguel.dungao@gmail.com>
2024-01-28 11:50:08 -0500

    made circle bigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 // Write your Color component here
-
+import { useState } from "react";
+import Color from "../components/Color/Color";
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+  const selectColorHandler = (color) => {
+    setSelectedColor(color);
+  };
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          selectedColor={selectedColor}
+          selectColorHandler={selectColorHandler}
+          color="red"
+        ></Color>
+        <Color
+          selectedColor={selectedColor}
+          selectColorHandler={selectColorHandler}
+          color="blue"
+        ></Color>
+        <Color
+          selectedColor={selectedColor}
+          selectColorHandler={selectColorHandler}
+          color="green"
+        ></Color>
+      </div>
     </div>
   );
 };
